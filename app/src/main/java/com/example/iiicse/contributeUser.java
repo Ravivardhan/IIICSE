dc255becb88bd5ba4d8cc4c15e24bae31b0f0499
Ravi vardhan <ravivardhanbtechcse@gmail.com>
2021-09-21 13:21:59 +0530

    livenotes

--- app/src/main/java/com/example/iiicse/contributeUser.java
+++ app/src/main/java/com/example/iiicse/contributeUser.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
@@ -19,8 +20,10 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -30,12 +33,12 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class contributeUser extends AppCompatActivity {
+    ArrayList<String>list=new ArrayList<>();
 
     Uri imageuri = null;
-    ArrayList<String> list=new ArrayList<>();
-
 
 
 
@@ -46,13 +49,13 @@ public class contributeUser extends AppCompatActivity {
         setContentView(R.layout.activity_contribute_user);
 
 
-
         Spinner dropdown = findViewById(R.id.spinner1);
         String[] items = new String[]{"LS", "PPL", "USP","CN","OOAD","MPMC"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
-        String unit=dropdown.getSelectedItem().toString();
+
+       // Toast.makeText(this, String.valueOf(list), Toast.LENGTH_SHORT).show();
 
 
 
@@ -62,28 +65,94 @@ public class contributeUser extends AppCompatActivity {
         upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent galleryIntent = new Intent();
-                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                EditText file= (EditText) findViewById(R.id.filename);
+                String filename=file.getText().toString();
+                if (filename.isEmpty()){
 
-                // We will be redirected to choose pdf
-                galleryIntent.setType("application/pdf");
-                startActivityForResult(galleryIntent, 1);
+                    Toast.makeText(contributeUser.this, "enter filename", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    String unit = dropdown.getSelectedItem().toString();
+                    list.add(unit);
+                    Intent galleryIntent = new Intent();
+                    galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
 
+                    // We will be redirected to choose pdf
+                    galleryIntent.setType("application/pdf");
+                    startActivityForResult(galleryIntent, 1);
+                }
+            }
+        });
 
+        Button sm=(Button)findViewById(R.id.button2);
+        sm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                DatabaseReference dref = FirebaseDatabase.getInstance().getReference("users");
+                String currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                String currentemail=FirebaseAuth.getInstance().getCurrentUser().getEmail();
 
-                FirebaseAuth mauth;
-                mauth=FirebaseAuth.getInstance();
-
-                DatabaseReference mref= FirebaseDatabase.getInstance().getReference("contribution").child(unit);
-
-                DatabaseReference mref2=FirebaseDatabase.getInstance().getReference("users");
-
-                mref2.child(mauth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+                dref.addChildEventListener(new ChildEventListener() {
                     @Override
-                    public void onDataChange(@NonNull  DataSnapshot snapshot) {
-                        String username=snapshot.child("username").getValue().toString();
-                        list.add(0,username);
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                        //String value = dataSnapshot.getValue(String.class);
+                        //list.add(String.valueOf(map));
+
+                        //System.out.println(map.get("username").toString());
+                        String user= String.valueOf(map.get("username"));
+                        String email= String.valueOf(map.get("email"));
+                        if(email.equals(currentemail)){
+                            list.add(user);
+                        }
+                        //Toast.makeText(contributeUser.this, String.valueOf(list), Toast.LENGTH_SHORT).show();
+                        StorageReference storageReference = FirebaseStorage.getInstance().getReference("IIICSE");
+                        StorageReference st=storageReference.child(list.get(0)).child(list.get(1)+"."+"pdf");
+                        String link=String.valueOf(st.getDownloadUrl());
+
+                        st.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Log.e("Tuts+", "uri: " + uri.toString());
+                                list.add(uri.toString());
+                                //Toast.makeText(contributeUser.this, String.valueOf(list), Toast.LENGTH_SHORT).show();
+                                //Handle whatever you're going to do with the URL here
+                                Pdfinfo info=new Pdfinfo(list.get(2),list.get(3),list.get(0),list.get(1));
+                                DatabaseReference dre = FirebaseDatabase.getInstance().getReference("contribution");
+                                //Toast.makeText(contributeUser.this, list.get(0), Toast.LENGTH_SHORT).show();
+                                dre.child(String.valueOf(list.get(0))).child(String.valueOf(list.get(1))).setValue(info);
+
+
+                                Intent i=new Intent(contributeUser.this,contribution.class);
+                                Toast.makeText(contributeUser.this, "thanks for the contribution", Toast.LENGTH_SHORT).show();
+                                startActivity(i);
+
+
+
+                            }
+                        });
+
+
+                        //Toast.makeText(contributeUser.this, link, Toast.LENGTH_SHORT).show();
+                        //System.out.println(link);
+
+
+
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull  DataSnapshot snapshot, @Nullable  String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull  DataSnapshot snapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull  DataSnapshot snapshot, @Nullable  String previousChildName) {
 
                     }
 
@@ -94,15 +163,7 @@ public class contributeUser extends AppCompatActivity {
                 });
 
 
-                Pdfinfo info=new Pdfinfo(list.get(0),list.get(1),unit,list.get(2));
-
-                mref2.child(list.get(2)).setValue(info);
-
-            }
-        });
-
-
-    }
+    }});}
     ProgressDialog dialog;
 
     @Override
@@ -129,10 +190,11 @@ public class contributeUser extends AppCompatActivity {
 
             EditText filename=findViewById(R.id.filename);
             String file=filename.getText().toString();
+            list.add(file);
+            Toast.makeText(this, String.valueOf(list), Toast.LENGTH_SHORT).show();
             // Here we are uploading the pdf in firebase storage with the name of current time
             final StorageReference filepath = storageReference1.child(file + "." + "pdf");
-            list.add(2,file);
-            Toast.makeText(contributeUser.this, filepath.getName(), Toast.LENGTH_SHORT).show();
+           Toast.makeText(contributeUser.this, filepath.getName(), Toast.LENGTH_SHORT).show();
             filepath.putFile(imageuri).continueWithTask(new Continuation() {
                 @Override
                 public Object then(@NonNull Task task) throws Exception {
@@ -144,7 +206,11 @@ public class contributeUser extends AppCompatActivity {
             }).addOnCompleteListener(new OnCompleteListener<Uri>() {
                 @Override
                 public void onComplete(@NonNull Task<Uri> task) {
+                    FirebaseAuth mauth;
+                    mauth=FirebaseAuth.getInstance();
+
                     if (task.isSuccessful()) {
+                        ArrayList<String> list=new ArrayList<>();
 
                         // After uploading is done it progress
                         // dialog box will be dismissed
@@ -152,15 +218,31 @@ public class contributeUser extends AppCompatActivity {
                         Uri uri = task.getResult();
                         String myurl;
                         myurl = uri.toString();
-                        list.add(1,myurl);
-
+                        list.add(myurl);
+                        System.out.println(myurl);
+                        Toast.makeText(contributeUser.this, String.valueOf(list), Toast.LENGTH_SHORT).show();
 
                         Toast.makeText(contributeUser.this, "Uploaded Successfully", Toast.LENGTH_SHORT).show();
+
+
+
+
                     } else {
                         dialog.dismiss();
                         Toast.makeText(contributeUser.this, "UploadedFailed", Toast.LENGTH_SHORT).show();
                     }
+
+
                 }
             });
+
+
+
         }
+
+
+
+
+
+
     }}
--- app/src/main/java/com/example/iiicse/imp.java
+++ app/src/main/java/com/example/iiicse/imp.java
@@ -2,13 +2,88 @@ package com.example.iiicse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class imp extends AppCompatActivity {
+    ArrayList<String> list=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imp);
+
+        ListView live=(ListView)findViewById(R.id.liveview);
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        DatabaseReference dref = FirebaseDatabase.getInstance().getReference("");
+        DatabaseReference ref=dref.child("LiveNotes");
+
+        //today date in java
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+        System.out.println(formatter.format(date));
+
+        ref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                Toast.makeText(imp.this, String.valueOf(map), Toast.LENGTH_SHORT).show();
+                //String value = dataSnapshot.getValue(String.class);
+                //list.add(String.valueOf(map));
+                System.out.println(map.toString());
+                System.out.println(map);
+                for (Map.Entry<String, Object> entry : map.entrySet())
+                    System.out.println("Key = " + entry.getKey() +
+                            ", Value = " + entry.getValue());
+                
+
+                live.setAdapter(adapter);
+
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+
     }
 }
